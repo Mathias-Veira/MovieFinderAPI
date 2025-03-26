b5d2e67d681029080209454a1d5af1b733abf9ca
Mathias-Veira <mveirad@gmail.com>
2025-03-26 11:12:39 +0100

    initial commit Spring project

---
+++ movieFinderAPI/src/main/java/com/movieFinder/movieFinderAPI/MovieFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.movieFinder.movieFinderAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinderApiApplication.class, args);
+	}
+
+}
