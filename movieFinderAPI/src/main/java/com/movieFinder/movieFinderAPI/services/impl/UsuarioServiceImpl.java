99280c8a9c3fe58963066d4798e25030d8bfb73e
Mathias-Veira <mveirad@gmail.com>
2025-03-28 12:27:25 +0100

    refactoring user service

--- movieFinderAPI/src/main/java/com/movieFinder/movieFinderAPI/services/impl/UsuarioServiceImpl.java
+++ movieFinderAPI/src/main/java/com/movieFinder/movieFinderAPI/services/impl/UsuarioServiceImpl.java
@@ -1,4 +1,5 @@
 package com.movieFinder.movieFinderAPI.services.impl;
+
 import com.movieFinder.movieFinderAPI.dtos.AccesoDTO;
 import com.movieFinder.movieFinderAPI.dtos.UsuarioDTO;
 import com.movieFinder.movieFinderAPI.error.IdNotFoundException;
@@ -14,59 +15,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import java.awt.dnd.InvalidDnDOperationException;
+
 @Service
 @Lazy
 public class UsuarioServiceImpl implements UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+
     /**
      * Método que permite comprobar el acceso de sesión de un usuario
+     *
      * @param accesoDTO Información para el acceso de sesión
      * @return Información del usuario si se cumple el acceso
      */
     @Override
     public UsuarioDTO comprobarAcceso(AccesoDTO accesoDTO) throws IncompleteDataException, LoginException, IdNotFoundException {
-        if(StringUtils.isBlank(accesoDTO.getNombreUsuario())||StringUtils.isBlank(accesoDTO.getPasswordUsuario())){
+        if (StringUtils.isBlank(accesoDTO.getNombreUsuario()) || StringUtils.isBlank(accesoDTO.getPasswordUsuario())) {
             throw new IncompleteDataException("El nombre de usuario y contraseña son obligatorios y no pueden estar vacíos");
         }
-        UsuarioDTO usuarioDTO = null;
-        try {
-            Usuario user = usuarioRepository.findByName(accesoDTO.getNombreUsuario());
-
-            if(!user.equals(null)){
-                Usuario usuario = user;
-                if(usuario.getPasswordUsuario().equals(accesoDTO.getPasswordUsuario())){
-                    usuarioDTO = UsuarioMapper.convertirADTO(usuario);
-                }else{
-                    //LoginException
-                    throw new RuntimeException();
-                }
-            }else{
-                //UserNotFoundException
-                throw new InvalidDnDOperationException();
-            }
-        }catch (InvalidDnDOperationException exception){
+        Usuario user = usuarioRepository.findByName(accesoDTO.getNombreUsuario());
+        if(user == null){
             throw new IdNotFoundException("El usuario no existe");
-        }catch (RuntimeException ex){
-            throw new LoginException("Nombre de usuario o contraseña incorrectos");
-        }catch (Exception e){
         }
-        return usuarioDTO;
+        if (!user.getPasswordUsuario().equals(accesoDTO.getPasswordUsuario())) {
+            //LoginException
+            throw new LoginException("Contraseña incorrecta");
+        }
+        return UsuarioMapper.convertirADTO(user);
     }
+
     /**
      * Método que permite registrar un usuario a partir de la información indicada
+     *
      * @param usuarioDTO Información del usuario a registrar
      * @return Se devuelve la información correspondiente al usuario posteriormente al registro
      */
     @Override
     public UsuarioDTO crearUsuario(UsuarioDTO usuarioDTO) throws IncompleteDataException, UserExistsException {
         UsuarioDTO user;
-        if(StringUtils.isBlank(usuarioDTO.getNombreUsuario())||StringUtils.isBlank(usuarioDTO.getPasswordUsuario())){
+        if (StringUtils.isBlank(usuarioDTO.getNombreUsuario()) || StringUtils.isBlank(usuarioDTO.getPasswordUsuario())) {
             throw new IncompleteDataException("El nombre de usuario y contraseña son obligatorios y no pueden estar vacíos");
         }
         user = obtenerUsuario(usuarioDTO.getNombreUsuario());
-        if(user != null){
+        if (user != null) {
             throw new UserExistsException("El usuario ya existe");
         }
 
@@ -75,12 +66,13 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     /**
      * Este método permite obtener un usuario por su nombre
+     *
      * @param name nombre del usuario a obtener
      * @return objeto UsuarioDTO
      */
     @Override
     public UsuarioDTO obtenerUsuario(String name) {
-        if(usuarioRepository.findByName(name) == null){
+        if (usuarioRepository.findByName(name) == null) {
             return null;
         }
         return UsuarioMapper.convertirADTO(usuarioRepository.findByName(name));
