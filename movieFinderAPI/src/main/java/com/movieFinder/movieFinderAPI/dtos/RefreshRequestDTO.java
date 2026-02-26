f08b0d1a9d874481c14ce308f74a7b070949d23c
Mathias-Veira <mveirad@gmail.com>
2026-02-26 12:16:02 +0100

    add DTO to send token to client

---
+++ movieFinderAPI/src/main/java/com/movieFinder/movieFinderAPI/dtos/RefreshRequestDTO.java
@@ -0,0 +1,21 @@
+package com.movieFinder.movieFinderAPI.dtos;
+
+public class RefreshRequestDTO {
+
+    private String refreshToken;
+
+    public RefreshRequestDTO() {
+    }
+
+    public RefreshRequestDTO(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
